966548d05cf7c2254bb1b53c35a2cc370cc81b38
Julia <pasichnyk.julia2@gmail.com>
2024-05-05 15:06:58 +0200

    update and new home work

--- script/script.js
+++ script/script.js
@@ -71,3 +71,82 @@ const modalButton = document.querySelector(".btn__fetch");
 modalButton.addEventListener("click", function () {
   modalApi();
 });
+
+//----//   Результат фильтрации в вывести на страницу во втором модальном окне на вашем сайте pallas cat studio. результат выложить на гитхаб в прошлый проект, но разделите новый код комментариями!!
+
+//   // Display the filtered names
+//   const filteredEmployeesDiv = document.getElementById("filteredEmployees");
+//   filteredEmployeesDiv.innerHTML = filteredNames.join(", ");
+// }
+
+class Employee {
+  constructor(name, age, position) {
+    this.name = name;
+    this.age = age;
+    this.position = position;
+  }
+
+  get() {
+    return `Имя сотрудника: ${this.name}, Возвраст сотрудника: ${this.age}, должность сотрудника: ${this.position}`;
+  }
+}
+
+const employees = [
+  new Employee("Боб", 26, "Инженер"),
+  new Employee("Алла", 28, "Врачь"),
+  new Employee("Скай", 31, "Системный администратор"),
+];
+
+function filteredEmployeesPosition(employees, position) {
+  const filteredEmployees = employees.filter((employee) => {
+    return employee.position === position;
+  });
+  const names = filteredEmployees.map(function (employee) {
+    return employee.name;
+  });
+  return names;
+}
+
+const buttonEmployee = document.querySelector(".btn__employee");
+
+buttonEmployee.addEventListener("click", function () {
+  const filteredNames = filteredEmployeesPosition(employees, "Врачь");
+  console.log(filteredNames);
+  modalEmployer(filteredNames);
+});
+
+function modalEmployer(filteredNames) {
+  const modalWindow = document.createElement("div");
+  modalWindow.classList.add("modalW");
+
+  const modalCloseWindow = document.createElement("div");
+  modalCloseWindow.classList.add("modalW__close");
+
+  const modalTitleWindow = document.createElement("h3");
+  modalTitleWindow.textContent = "Employee information:";
+
+  const modalWindowContent = document.createElement("div");
+  modalWindowContent.classList.add("modalWindow__content");
+
+  const nameList = document.createElement("ul");
+
+  filteredNames.forEach((name) => {
+    const listName = document.createElement("li");
+    listName.textContent = name;
+    nameList.append(listName);
+  });
+
+  modalWindowContent.append(nameList);
+  modalWindow.append(modalCloseWindow, modalTitleWindow, modalWindowContent);
+
+  const backgroundWindow = document.createElement("div");
+  backgroundWindow.classList.add("background__window");
+  backgroundWindow.append(modalWindow);
+
+  const positionModal = document.querySelector(".position__modal");
+  positionModal.append(backgroundWindow);
+
+  modalCloseWindow.addEventListener("click", function () {
+    backgroundWindow.remove();
+  });
+}
